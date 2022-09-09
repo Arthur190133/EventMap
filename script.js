899d82939ded22bb48a75e5f9bfea767526000c3
Arthur190133 <190133@site.asty-moulin.be>
2022-09-09 21:23:09 +0200

    tt

--- script.js
+++ script.js
@@ -56,6 +56,22 @@ function FocusOnMarker(MarkerButtonId){
     }
     else{
       console.log("cannot find a marker at index : " + MarkerId);
+      document.getElementById("MasterContent").innerHTML += "<p> Cannot find your event, please try later </p>";
+      let xhttp;
+      let element = document.getElementById("MasterContent");
+      let file = "EventButtonCardError.html";
+      if (file){
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+          if(this.readyState == 4) {
+            if(this.status == 200) {element.innerHTML == this.responseText;}
+            if(this.status == 404) {element.innerHTML = "<h1>Page not found. </h1>";}
+          }
+        }
+        xhttp.open("GET", 'templates/${file}', true);
+        xhttp.send();
+        return;
+      }
     }
 
   }
