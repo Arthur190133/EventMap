411942cd73ab132f08ea8e6dc7fcfa0ea3723289
gregorymartin200261 <200261@site.asty-moulin.be>
2023-02-06 20:22:41 +0100

    here

--- Pages/User/UserProfileEventCard.php
+++ Pages/User/UserProfileEventCard.php
@@ -1,14 +1,9 @@
-<div class="window">
-    <div class="align">
-        <span class="red"></span>
-        <span class="yellow"></span>
-        <span class="green"></span>
-    </div>
-
-    <h1><?= $Event->Name ?></h1>
-    <div>
-        <p>prix : <?= $Event->Price;  ?>$<p>
+<article class="window">
+<div class="window_content">
+    <span class="window_title"><?= $Event->Name ?></span>
+        <span class="window_subtitle"><p>prix : <?= $Event->Price;  ?>$<p>
         <p>EventStartDate : <?= $Event->StartDate ?><p>
         <p>EventEndDate : <?= $Event->EndDate ?><p>
-    </div>
+    </span>
 </div>
+</article>
