f53e14066059e8d02590f8fbc245946012eee34b
gregorymartin200261 <200261@site.asty-moulin.be>
2023-03-21 19:19:33 +0100

    profile modif

--- Pages/User/UserProfile.php
+++ Pages/User/UserProfile.php
@@ -1,5 +1,10 @@
 <?php
     $user = $_SESSION['user'];
+
+    if(isset($_POST['btnModif']))
+    {
+        $_SESSION['user']=modifUserInfo();       
+    }
 ?>
 
 <link href= "\css\userProfile.css" rel="stylesheet">
@@ -13,11 +18,42 @@
                     <div class="card">
                         <div class="card__content">
                             <span class="img"></span>
-                            <h2><?= $user->UserFirstName ?> <?= $user->UserName ?></h2>
-                            <p><?= $user->UserDescription ?></p>
-                            <button> MODIFIER </button>
-                            
+                            <form method="post">
+
+                                <?php if(!isset($_POST['submit'])) : ?>
+                                    <h2><?= $user->UserFirstName ?> <?= $user->UserName ?></h2>
+                                    <p><?= $user->UserDescription ?></p>
+                                    <button id="submit" name="submit" value="submit"> MODIFIER </button>
+                                <?php else : ?>
+                                    <div class="input-group">
+                                        <label class="label">Prenom</label>
+                                        <input type="text" class="input" name="newUserFirstName" placeholder="Prenom" value="<?=$_SESSION['user']->UserFirstName?>">
+                                        <div>
+
+                                        </div>
+                                    </div>
+                                    <div class="input-group">
+                                        <label class="label">Nom</label>
+                                        <input type="text" class="input" name="newUserLastName" placeholder="Nom" value="<?=$_SESSION['user']->UserName?>">
+                                        <div>
+                                            
+                                        </div>
+                                    </div>
+                                    <div class="input-group">
+                                        <label class="label">Description</label>
+                                        <input type="text" class="input" name="newUserDescription" placeholder="Description" value="<?=$_SESSION['user']->UserDescription?>">
+                                        <div>
+                                            
+                                        </div>
+                                    </div>
+                                    <button class="button2 id="btnModif" name="btnModif" value="nouveau">Enregistrer</button>
+                                <?php endif?>
+                                        
+                            </form>
+                                        
                         </div>
+                        <div class="blob"></div>
+                        <div class="blob"></div>
                         <div class="blob"></div>
                         <div class="blob"></div>
                         <div class="blob"></div>
@@ -48,11 +84,12 @@
                 <div class="droite">
                     <div class="NewEventBackground">
                         <div class="blob"></div>
-                            <p class="NewEvent">NOUVEL</p><p class="NewEvent">EVENT</p>
-                            <button>
+                        <p class="NewEvent">NOUVEL</p><p class="NewEvent">EVENT</p>
+                        <form action="post">
+                            <a class="button" href="/Event/RegisterEvent.php">
                                 <span>CREER</span>
-                            </button>
-                            
+                            </a>   
+                        </form>
                     </div>
                 </div>
             </div>
